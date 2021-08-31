c8a6a7e8d35914aba3abfd888aac5569e2400374
Viktor <fomvaf@mail.ru>
2021-08-31 23:56:14 +0300

    new

---
+++ application/db/people.py
@@ -0,0 +1,6 @@
+def get_employees():
+    print('Выводим информацию по сотрудникам')
+
+
+if __name__ == '__main__':
+    get_employees()
---
+++ application/salary.py
@@ -0,0 +1,6 @@
+def calculate_salary():
+    print('Выводим информацию по зарплате')
+
+
+if __name__ == '__main__':
+    calculate_salary()
